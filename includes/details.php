7eda478519694839ee515b36ee3c8f5777ab6d1d
Carrington-dev <crn96m@gmail.com>
2024-11-20 07:58:19 +0200

    Added clix Project Initial SetUp

---
+++ includes/details.php
@@ -0,0 +1,6 @@
+<?php
+    $company = "Clix Projects";
+    $tel = "+27 67 042 1269";
+    $email = "info@clixprojects.co.za";
+    $address = "868 Sparrow Crescent Street<br>Riverside View, Johannesburg";
+?>
